e6e41bcf55441d65ec7a636ee93c72987ab0dbc2
Your Name <you@example.com>
2018-03-06 15:06:31 +0530

    first commit

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ module.exports = {
 			"Description":"It is also called polytechnic. It diploma level engineering courses.",
 			"courses":["Computer Science Engineering","Mechanical Engineering","Civil Engineering","Electronics and Communication Engineering", "Electrical Engineering"]
 		},
-		"ITI":{"Description":"","courses":""},
-		"Army":{"Description":"","courses":""},
-		"Navy":{"Description":"","courses":""},
-		"Police":{"Description":"","courses":""},
-		"SSC":{"Description":"","courses":""}
+		"ITI":{"Description":"ITI courses are designed to impart basic skills needed for a specified trade like electrician, machinist, fitter, plumber, turner, welder etc. Constituted under Ministry of Labour, Union Government of India, Government-run ITIs and privately-run ITCs are training organisations which provide training in technical field. After passing the course a person may opt to undergo practical training like from NCVT (National Council for Vocational training ) in his trade in an industry","courses":["Indian Railways", "telecom departments"]},
+		"Navy":{"Description":"Job opportunities are available in Indian Navy on the basis of written examination","courses":["Dock Yard Apprentices","sailors","Artificer Apprentices"]},
+		"Army":{"Description":"One can join in Indian Army to the posts of soldiers in technical trade through written exams.","courses":["Indian Army Soldier Clerks Examination, Indian Army Soldier General Duty (N.E.R.)", "Indian Army Soldier Technical (M.E.R.)", "Indian Army Soldier Nursing Assistants (M.E.R.)"]},
+		"Police Force":{"Description":"On the basis of physical test and written test, one can join as constable.","courses":["Central Reserve Police Force (CRPF)"]},
+		"SSC":{"Description":"Staff Selection Commission (SSC) conducts some job examination for those who passed matriculation","courses":["clerks"]}
 	},
 	"Intermediate":{
 		"mpc":{},
